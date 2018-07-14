3d5ff0d93ac2bc5ebb5e72645093d8dc28a2ab50
MUHAMMAD FAISAL SHABBIR <mfaisalshabbir87@gmail.com>
2018-07-14 14:33:14 -0400

    deleting file

--- Sentiment-Analysis-Project/Instructions/config.py
+++
@@ -1,5 +0,0 @@
-consumer_key = "ZiJZifT8sGLOHSIiPhZvr9p4T"
-consumer_secret = "sIMXFPmAFT9664uV6se9vnfGaY29A7J6ixc3XhPyByaT6BfILe"
-access_token = "1013065852147691522-OtTuEKQHrX7ixOC0VWI53k26Ln22HY"
-access_token_secret = "JM1qXMwVbJDWMtjFnk17v5PCpz6ohvTIrywCYJ1Dm5LZB"
-
